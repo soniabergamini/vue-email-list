53822293138a8947110859151c3c1e88b09d361d
soniabergamini <sounia96@outlook.it>
2023-05-17 14:39:12 +0200

    feat(js): print a list of 10 emails in console

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            myString: "string",
+            booleanAPI: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            emailList: [],
         }
     },
     methods: {
-        // Print a string in console
-        test() {
-            console.log(this.myString)
+        // Print a List of 10 email addresses in console
+        emailApiCall() {
+            for (let i = 0; i < 10; i++) {
+                axios.get(this.booleanAPI).then((response) => {
+                    this.emailList.push(response.data)
+                    console.log("single email list:", this.emailList)
+                });
+            }
+            setTimeout(() => console.log("total email list:", this.emailList), 1 * 2000);
         }
     },
     mounted() {
         console.log("Hello from VueJS 👋")
+        this.emailApiCall()
     }
 }).mount('#app')
